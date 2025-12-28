353563f893ca74c7deae1e467abb58acea4c006d
BINIS\binis <binisbelev@gmail.com>
2025-12-28 16:08:48 +0200

    Added map backed projection

---
+++ src/main/java/net/binis/codegen/projection/provider/MapBackedProjectionProvider.java
@@ -0,0 +1,173 @@
+package net.binis.codegen.projection.provider;
+
+import net.binis.codegen.factory.CodeFactory;
+import net.binis.codegen.map.Mapper;
+import net.binis.codegen.projection.interfaces.CodeProxyControl;
+import net.binis.codegen.projection.objects.CodeProxyBase;
+import net.binis.codegen.tools.Reflection;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.modifier.Visibility;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.scaffold.TypeValidation;
+import net.bytebuddy.implementation.FieldAccessor;
+import net.bytebuddy.implementation.MethodCall;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.*;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Objects.isNull;
+import static net.binis.codegen.projection.tools.ProjectionTools.decapitalize;
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+public class MapBackedProjectionProvider {
+
+    private static final Map<Class<?>, Class<?>> CLASS_CACHE = new ConcurrentHashMap<>();
+    private static final ByteBuddy BYTE_BUDDY = new ByteBuddy()
+            .with(TypeValidation.DISABLED);
+
+    @SuppressWarnings("unchecked")
+    public static <T> T create(Class<T> projection, Map<String, Object> map) {
+        try {
+            var proxyClass = getOrCreateProxyClass(projection);
+            var instance = CodeFactory.create(proxyClass, map);
+            return (T) instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create projection instance for " + projection.getName(), e);
+        }
+    }
+
+    private static Class<?> getOrCreateProxyClass(Class<?> projection) {
+        return CLASS_CACHE.computeIfAbsent(projection, cls -> {
+            try {
+                return BYTE_BUDDY
+                        .subclass(TypeDescription.Generic.Builder
+                                .parameterizedType(CodeProxyBase.class, Map.class)
+                                .build())
+                        .name(cls.getName() + "$MapBackedProxy")
+                        .implement(cls)
+                        .defineConstructor(Visibility.PUBLIC)
+                        .withParameters(Map.class)
+                        .intercept(MethodCall.invoke(CodeProxyBase.class.getDeclaredConstructor())
+                                .andThen(FieldAccessor.ofField("value").setsArgumentAt(0)))
+                        .method(any())
+                        .intercept(MethodDelegation.to(MapInterceptor.class))
+                        .make()
+                        .load(projection.getClassLoader())
+                        .getLoaded();
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create proxy class for " + projection.getName(), e);
+            }
+        });
+    }
+
+    public static class MapInterceptor {
+
+        @RuntimeType
+        public static Object intercept(
+                @FieldValue("value") Map<String, Object> map,
+                @Origin Method method,
+                @AllArguments Object[] args) {
+
+            var name = method.getName();
+            int paramCount = method.getParameterCount();
+
+            if (paramCount == 0) {
+                if (name.startsWith("get") && name.length() > 3) {
+                    return handleGetter(map, method, name.substring(3));
+                } else if (name.startsWith("is") && name.length() > 2) {
+                    return map.get(decapitalize(name.substring(2)));
+                } else if (name.equals("toString")) {
+                    return map.toString();
+                } else if (name.equals("hashCode")) {
+                    return map.hashCode();
+                }
+            }
+
+            if (paramCount == 1 && name.startsWith("set") && name.length() > 3) {
+                map.put(decapitalize(name.substring(3)), args[0]);
+                return null;
+            }
+
+            if (paramCount == 1 && name.equals("equals")) {
+                return handleEquals(map, args[0]);
+            }
+
+            throw new UnsupportedOperationException("Method not supported: " + method);
+        }
+
+        @SuppressWarnings("unchecked")
+        private static Object handleGetter(Map<String, Object> map, Method method, String propertyName) {
+            var key = decapitalize(propertyName);
+            var result = map.get(key);
+
+            if (isNull(result)) {
+                if (method.getReturnType().isPrimitive()) {
+                    if (int.class.equals(method.getReturnType())) {
+                        return 0;
+                    }
+                    if (long.class.equals(method.getReturnType())) {
+                        return 0L;
+                    }
+                    if (double.class.equals(method.getReturnType())) {
+                        return 0D;
+                    }
+                    if (float.class.equals(method.getReturnType())) {
+                        return 0F;
+                    }
+                    if (boolean.class.equals(method.getReturnType())) {
+                        return false;
+                    }
+                    if (byte.class.equals(method.getReturnType())) {
+                        return (byte) 0;
+                    }
+                    if (short.class.equals(method.getReturnType())) {
+                        return (short) 0;
+                    }
+                    if (char.class.equals(method.getReturnType())) {
+                        return (char) 0;
+                    }
+                } else {
+                    return null;
+                }
+            }
+
+            Class<?> returnType = method.getReturnType();
+
+            if (returnType.isAssignableFrom(result.getClass())
+                    && !Collection.class.isAssignableFrom(result.getClass())) {
+                return result;
+            }
+
+            if (result instanceof List list && List.class.equals(returnType)) {
+                var type = ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0];
+                if (!Object.class.equals(type)) {
+                    var cls = Reflection.loadClass(((Class<?>) type).getName());
+                    return list.stream().map(o -> Mapper.convert(o, cls)).toList();
+                }
+            }
+
+            return Mapper.convert(result, returnType);
+        }
+
+        private static boolean handleEquals(Map<String, Object> map, Object other) {
+            if (other == null) return false;
+
+            if (other instanceof CodeProxyControl control) {
+                return map.equals(control._object$());
+            }
+
+            if (other instanceof CodeProxyBase<?> base) {
+                Object unwrapped = CodeProxyBase.unwrap(other);
+                return map.equals(unwrapped);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/test/java/net/binis/codegen/projection/MapBackedProjectionTest.java
@@ -0,0 +1,61 @@
+package net.binis.codegen.projection;
+
+/*-
+ * #%L
+ * code-generator-projection
+ * %%
+ * Copyright (C) 2021 - 2024 Binis Belev
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import lombok.extern.slf4j.Slf4j;
+import net.binis.codegen.projection.provider.MapBackedProjectionProvider;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+class MapBackedProjectionTest {
+
+    private static final Map<String, Object> map = Map.of(
+            "int", "5",
+            "string", 6.0,
+            "double", "7",
+            "long", "8"
+    );
+
+    public interface Projection {
+        int getInt();
+        String getString();
+        double getDouble();
+        long getLong();
+        float getFloat();
+    }
+
+    @Test
+    void test() {
+        var p = MapBackedProjectionProvider.create(Projection.class, map);
+
+        assertEquals(5, p.getInt());
+        assertEquals("6.0", p.getString());
+        assertEquals(7.0, p.getDouble());
+        assertEquals(8L, p.getLong());
+        assertEquals(0.0, p.getFloat());
+    }
+
+}
